2d3bcdbb7eadd6b5ff3a77544803b8e9dc9f21ae
MechanicSCB <kolek-ulyanov@yandex.ru>
2021-01-26 15:50:42 +0300

    riddle show refactor

--- resources/views/riddles/card.blade.php
+++ resources/views/riddles/card.blade.php
@@ -1,4 +1,6 @@
-<div class="card flex flex-col"  style="height: 200px" >
+<div class="card flex flex-col"
+{{--     style="height: 200px" --}}
+>
     <h3 class="font-normal text-xl py-4 border-l-4 border-blue pl-4 -ml-5 mb-3">
         <a href="{{ route('riddles.show', [$riddle->id]) }}" class="text-default no-underline">{{ $riddle->title }}</a>
     </h3>
--- resources/views/riddles/show.blade.php
+++ resources/views/riddles/show.blade.php
@@ -1,20 +1,67 @@
-<x-app-layout>
-    <x-slot name="header">
+@extends('my_layouts.app')
 
-    </x-slot>
+@section('content')
+    @php
+        /**
+         * @var $riddle App\riddle
+         */
+    @endphp
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    @php /** @var  App\Models\Riddle $riddle  */ @endphp
-                    <div class="container">
-                        <h3>{{ $riddle->title }}</h3>
-                        <p>{{ $riddle->description }}</p>
-                        <p>{{ $riddle->solution }}</p>
-                    </div>
+    <header class="flex items-center mb-3 py-4">
+        <div class="flex justify-between items-end w-full">
+            <p class="text-default text-sm font-normal">
+                <a href="/riddles" class="text-default text-sm font-normal no-underline">My riddles</a>  / {{ $riddle->title }}
+            </p>
+            <!-- Invited Users -->
+            <div class="flex items-center">
+                <theme-switcher></theme-switcher>
+
+            </div>
+
+        </div>
+    </header>
+
+    <main>
+        <div class="lg:flex -mx-3">
+            <div class="lg:w-3/4 px-3 mb-6">
+                <div class="mb-8">
+                    <h2 class="text-lg text-default font-normal mb-3">Riddle</h2>
+                    @include('riddles.card')
+                    {{-- tasks --}}
+                </div>
+                <div>
+                    <h2 class="text-lg text-default font-normal mb-3">Solution</h2>
+
+                    {{-- general notes --}}
+                    <form method="POST" action="#">
+                        @csrf
+                        @method('PATCH')
+                        <textarea
+                            class="card w-full mb-4"
+                            style="min-height: 200px"
+                            placeholder="Anything special tht you want to make a note of?"
+                            name="notes"
+                        ></textarea>
+
+                        <button type="submit" class="button">Save</button>
+                    </form>
+
+{{--                    @include('errors')--}}
+
                 </div>
             </div>
+
+            <div class="lg:w-1/4 px-3 lg:py-8">
+                @include('riddles.card')
+{{--                @include('riddles.activity.card')--}}
+
+{{--                @can('manage', $riddle)--}}
+{{--                    @include('riddles.invite')--}}
+{{--                @endcan--}}
+            </div>
         </div>
-    </div>
-</x-app-layout>
+    </main>
+
+
+
+@endsection
